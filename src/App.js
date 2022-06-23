a9f79fa1714c80910f1f1f476ec802f4cce4ac9f
Sirjamo1 <jam_how@hotmail.com>
2022-06-23 13:12:52 +1000

    added boiler plate

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Header from "./components/Header.js";
+
+export default function App() {
+  return (
+    <div className="card--main-container">
+      <Header />
+    </div>
+  );
+}
