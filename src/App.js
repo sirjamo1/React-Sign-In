e6d0d39341240d0c36c2ca4dc9ac9f3fdfd60d64
Sirjamo1 <jam_how@hotmail.com>
2022-06-23 14:44:55 +1000

    coded along with youtube

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
 import React from "react";
 import Header from "./components/Header.js";
+import LoginForm from "./components/LoginForm";
 
 export default function App() {
+  const adminUser = {
+    email: "admin@admin",
+    password: "admin123",
+  };
+  const [user, setUser] = React.useState({ name: "", email: "" });
+  const [error, setError] = React.useState("");
+
+  const Login = (details) => {
+    console.log(details);
+    if (
+      details.email == adminUser.email &&
+      details.password == adminUser.password
+    ) {
+      console.log("logged in");
+      setUser({
+        name: details.name,
+        email: details.email,
+      });
+    } else {
+      setError("Deatails do not match");
+    }
+  };
+
+  const Logout = () => {
+    setUser({ name: "", email: "" });
+  };
   return (
-    <div className="card--main-container">
-      <Header />
+    <div className="app">
+      {user.email != "" ? (
+        <div className="welcome">
+          <h2>
+            Welcome, <span>{user.name}</span>
+          </h2>
+          <button onClick={Logout}>Logout</button>
+        </div>
+      ) : (
+        <LoginForm Login={Login} error={error} />
+      )}
     </div>
   );
 }
---
+++ src/components/LoginForm.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+function LoginForm({ Login, error }) {
+  const [details, setDetails] = React.useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    Login(details);
+  };
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="form--inner">
+        <h2>Login</h2>
+        {error != "" ? <div className="error">{error}</div> : ""}
+        <div className="form--group">
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            onChange={(event) =>
+              setDetails({ ...details, name: event.target.value })
+            }
+            value={details.name}
+          />
+        </div>
+        <div className="form--group">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            onChange={(event) =>
+              setDetails({ ...details, email: event.target.value })
+            }
+            value={details.email}
+          />
+        </div>
+        <div className="form--group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            onChange={(event) =>
+              setDetails({ ...details, password: event.target.value })
+            }
+            value={details.password}
+          />
+        </div>
+        <input type="submit" value="LOGIN"></input>
+      </div>
+    </form>
+  );
+}
+
+export default LoginForm;
